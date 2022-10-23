3519843244e318cffcb50b834f607bd8c006538f
m00E10 <93796498+m00E10@users.noreply.github.com>
2022-10-23 03:52:48 +0000

    Add files via upload

--- Preprocessing/UsedPipeline/Pipeline.py
+++ Preprocessing/UsedPipeline/Pipeline.py
@@ -14,3 +14,24 @@ class Preprocessing:
 
     def histogram(self):
         return cv.equalizeHist(cv.cvtColor(cv.imread(self.img), cv.COLOR_BGR2GRAY))
+
+    def brighten(self, value=30):
+        h, s, v = cv.split(cv.cvtColor(cv.imread(self.img), cv.COLOR_BGR2HSV))
+
+        lim = 255 - value
+        v[v > lim] = 255
+        v[v <= lim] += value
+
+        return cv.cvtColor(cv.merge((h, s, v)), cv.COLOR_HSV2BGR)
+
+    def contrast(self, clip_limit=2.0, tile_grid_size=(8, 8)):
+        lab = cv.cvtColor(cv.imread(self.img), cv.COLOR_BGR2LAB)
+
+        l_channel, a, b = cv.split(lab)
+        clahe = cv.createCLAHE(clip_limit, tile_grid_size)
+        limg = cv.merge((clahe.apply(l_channel), a, b))
+
+        return cv.cvtColor(limg, cv.COLOR_LAB2BGR)
+
+    def bilateral(self, d=9, sigma_color=75, sigma_space=75):
+        return cv.bilateralFilter(cv.imread(self.img), d, sigma_color, sigma_space)
--- Preprocessing/UsedPipeline/imported_class.py
+++ Preprocessing/UsedPipeline/imported_class.py
@@ -6,7 +6,11 @@ def main():
     PreProcClass = Preprocessing('9.png')
     cv.imwrite('hist.png', PreProcClass.histogram())
     cv.imwrite('sharp.png', PreProcClass.sharpen())
+    cv.imwrite('brighten.png', PreProcClass.brighten())
     cv.imwrite('hist_sharp.png', Preprocessing('hist.png').sharpen())
+    cv.imwrite('contrast.png', PreProcClass.contrast())
+    cv.imwrite('bilateral.png', PreProcClass.bilateral())
+    cv.imwrite('bilateral_custom.png', PreProcClass.bilateral(75, 75))
 
 
 if __name__ == "__main__":
