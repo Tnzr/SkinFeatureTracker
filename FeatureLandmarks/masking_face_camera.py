ab99df3e8a0ddd12c2df1f295cb5202906f615c2
Aznviben <72636205+Aznviben@users.noreply.github.com>
2022-10-24 18:31:38 -0400

    issue fixed with masking_face_camera
    Issue was fixed, the information would actually crash when input was recieved. this is because face had been used for two different variables.

--- FeatureLandmarks/masking_face_camera.py
+++ FeatureLandmarks/masking_face_camera.py
@@ -19,7 +19,7 @@ predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 # read the image
 cap = cv2.VideoCapture(0)
 
-def getLipsMask(size, lips):
+def getMask(size, lips):
     # Find Convex hull of all points
     hullIndex = cv2.convexHull(np.array(lips), returnPoints=False)
     # Convert hull index to list of points
@@ -30,7 +30,7 @@ def getLipsMask(size, lips):
     mask = np.zeros((size[0], size[1], 3), dtype=np.uint8)
     cv2.fillConvexPoly(mask, np.int32(hullInt), (255, 255, 255))
     return mask
-
+n = 0
 while True:
     start_time = time.time() # start time of the loop
 
@@ -40,50 +40,37 @@ while True:
     im = frame
 
     # Use detector to find landmarks
-
-
-    imDlib = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+    imDlib = cv2.cvtColor(gray, cv2.COLOR_BGR2RGB)
     points = face.getLandmarks(detector, predictor, imDlib)
 
     if not points:
         continue
-    else:
+    ## Select point and create "mask"
+
+    face2 = 0
+    mouth = 0
+    righteye = 0
+    lefteye= 0
+    face2 = [points[x] for x in range(0, 28)]
+    mouth = [points[x] for x in range(48, 60)]
+    righteye = [points[x] for x in range(42, 48)]
+    lefteye = [points[x] for x in range(36, 41)]
+    mask = getMask(im.shape, face2)
+    left_eye_mask = getMask(im.shape, lefteye)
+    right_eye_mask = getMask(im.shape, righteye)
+    mouth_mask = getMask(im.shape, mouth)
 
 
-        ## Select points of lips and create "lips mask"
-        face = [points[x] for x in range(0, 28)]
-        mouth = [points[x] for x in range(48, 60)]
-        righteye = [points[x] for x in range(42, 48)]
-        lefteye = [points[x] for x in range(36, 41)]
-        clone_lips = frame.copy()
-        for point in face:
-            cv2.circle(clone_lips, point, 1, (0, 255, 0), -1)
-        for point in mouth:
-            cv2.circle(clone_lips, point, 1, (0, 0, 255), -1)
-        for point in lefteye:
-            cv2.circle(clone_lips, point, 1, (0, 0, 255), -1)
-        for point in righteye:
-            cv2.circle(clone_lips, point, 1, (0, 0, 255), -1)
-        clone_lips = cv2.cvtColor(clone_lips, cv2.COLOR_BGR2RGB)
-        contours = [np.asarray(face, dtype=np.int32)]
-        (x, y, w, h) = cv2.boundingRect(contours[0])
-        center = (int(x + w / 2), int(y + h / 2))
-        mask = getLipsMask(im.shape, face)
-        left_eye_mask = getLipsMask(im.shape, lefteye)
-        right_eye_mask = getLipsMask(im.shape, righteye)
-        mouth_mask = getLipsMask(im.shape, mouth)
-
-        new_mask = mask - mouth_mask - right_eye_mask - left_eye_mask
-        cv2.imshow("", new_mask)
-        cv2.waitKey(0)
+    new_mask = mask - mouth_mask - right_eye_mask - left_eye_mask
+    cv2.imshow("", new_mask)
 
     print("FPS: ", 1.0 / (time.time() - start_time)) # FPS = 1 / time to process loop
-    # Exit when escape is pressed
-    if cv2.waitKey(delay=1) == 27:
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27:
         break
 
     # When everything done, release the video capture and video write objects
-    cap.release()
+cap.release()
 
-    # Close all windows
-    cv2.destroyAllWindows()
+# Close all windows
+cv2.destroyAllWindows()
--- FeatureLandmarks/mediapipe_mask.py
+++ FeatureLandmarks/mediapipe_mask.py
@@ -3,15 +3,19 @@ import cv2 as cv
 import mediapipe as mp
 import numpy as np
 from matplotlib import pyplot as plt
-
+import time
+import inspect
 mpfacemesh = mp.solutions.face_mesh
 FaceMesh = mpfacemesh.FaceMesh(max_num_faces=1)
 mpdraw = mp.solutions.drawing_utils
 drawspec1 = mpdraw.DrawingSpec(color=(255, 255, 0), circle_radius=0, thickness=1)
 drawspec2 = mpdraw.DrawingSpec(color=(0, 255, 0), circle_radius=0, thickness=1)
 
+
+
 webcam = cv.VideoCapture(0)
 while True:
+    start_time = time.time() # start time of the loop
 
     scc, img = webcam.read()
     img = cv.flip(img, 1)
@@ -22,9 +26,14 @@ while True:
     if results.multi_face_landmarks:
         for face_lm in results.multi_face_landmarks:
             img = blank_img
+            print(inspect.getmembers(face_lm, lambda a:not(inspect.isroutine(a))))
+            
+
             mpdraw.draw_landmarks(img, face_lm,
-                                  mpfacemesh.FACEMESH_CONTOURS,
+                                  mpfacemesh.FACEMESH_TESSELATION,
                                   drawspec1, drawspec2)
+
+    print("FPS: ", 1.0 / (time.time() - start_time)) # FPS = 1 / time to process loop
 
     k = cv.waitKey(1)
     if k == ord('q'):
